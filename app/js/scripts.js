d0d10f35b1a1d1fbbce361534e1a15f66846aab7
xkored <xkored@gmail.com>
2019-07-08 15:50:50 +0400

    finished adapt

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,4 +1,8 @@
 $(function() {
+  //preloader
+  $(window).on('load', function () {
+      $('.preloader').fadeOut();
+  });
   $('.offer__slider').owlCarousel({
       nav: true,
       items: 1,
@@ -12,23 +16,46 @@ $(function() {
       loop: false,
       dots: false,
       navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
-      // responsive : {
-      //   0   : {
-      //       items: 1
-      //   },
-      //   380 : {
-      //       items: 1
-      //   },
-      //   480 : {
-      //       items: 1
-      //   },
-      //   768 : {
-      //       items: 3
-      //   },
-      //   1040 : {
-      //       items: 4
-      //   }
-      // }
+      responsive : {
+        0   : {
+            items: 1
+        },
+        380 : {
+            items: 1
+        },
+        480 : {
+            items: 1
+        },
+        768 : {
+            items: 3
+        },
+        1200 : {
+            items: 4
+        }
+      }
+  });
+  $('.cat__pop-slider').owlCarousel({
+      nav: true,
+      loop: false,
+      items: 2,
+      dots: false,
+      center: false,
+      margin: 20,
+      navText: ["<img src='img/slider__arrow_prev.png'>", "<img src='img/slider__arrow_next.png'>"],
+      responsive : {
+        0   : {
+            items: 1
+        },
+        380 : {
+            items: 1
+        },
+        480 : {
+            items: 1
+        },
+        1200 : {
+            items: 2
+        }
+      }
   });
 	// tabs 
   $(document).ready(function () {
@@ -43,11 +70,31 @@ $(function() {
       }
     });
   });
-   $('.tech__item').hover( function() {
+  $('.tech__item').hover( function() {
     $(this).toggleClass('active');
-   });
+  });
+  //cat item animation
+  $('.cat__item-wrp').hover( function() {
+    $(this).toggleClass('active');
+  });
+
+ $('.fancybox').fancybox({
+      afterClose: function () {
+         $('.cat__item-wrp').removeClass('active');
+      }
+  });
+
+  $('.fancybox-close-small').on('click', function () {
+    $('.cat__item-wrp').removeClass('active');
+  });
+  //cat popup accordeon
+  $('.cat__pop-acc-cnt').hide();
+  $('.cat__pop-acc-btn').on('click', function () {
+    $(this).toggleClass('active').siblings('.cat__pop-acc-cnt').slideToggle();
+  });
   //Закрываем AjaxForm popup после успешной отправки
   // $(document).on('af_complete', function(event,res) {
   //   if(res.success) parent.$.fancybox.close();
   // });
+
 });
